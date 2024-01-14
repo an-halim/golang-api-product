59be323de674f2f9a81f75ecf826a4e30c8378b4
an-halim <halimbla2@gmail.com>
2024-01-14 15:35:07 +0700

    feat: post data validator

---
+++ utils/validator.go
@@ -0,0 +1,15 @@
+package utils
+
+import "github.com/go-playground/validator/v10"
+
+type Product struct {
+	Name      string `validate:"required"`
+	Price     int    `validate:"required"`
+	Stock     int    `validate:"required"`
+	Image_url string `validate:"required"`
+}
+
+func ValidateProduct(product *Product) error {
+	validate := validator.New()
+	return validate.Struct(product)
+}
